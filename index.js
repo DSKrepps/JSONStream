0f0e5afaca835581c128d9754b024d0d916a6d06
Dylan Krepps <dskrepps@gmail.com>
2014-02-23 22:18:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,7 @@ var Parser = require('jsonparse')
   , Stream = require('stream').Stream
   , through = require('through')
 
-/*
-
-  the value of this.stack that creationix's jsonparse has is weird.
-
-  it makes this code ugly, but his problem is way harder that mine,
-  so i'll forgive him.
-
-*/
-
-exports.parse = function (path) {
+exports.parse = function (handler) {
 
   var parser = new Parser()
   var stream = through(function (chunk) {
@@ -32,156 +23,39 @@ exports.parse = function (path) {
     stream.queue(null)
   })
 
-  if('string' === typeof path)
-    path = path.split('.').map(function (e) {
-      if (e === '*')
-        return true
-      else if (e === '') // '..'.split('.') returns an empty string
-        return {recurse: true}
-      else
-        return e
-    })
 
+  parser.onValue = function(value){
 
-  var count = 0, _key
-  if(!path || !path.length)
-    path = null
+    if(value === Object(value))
+      return
 
-  parser.onValue = function () {
-    if (!this.root && this.stack.length == 1)
-      stream.root = this.value
+    var i = 0
+      , len = this.stack.length
+      , path = []
+      , stack = this.stack
 
-    if(! path) return
-
-    var i = 0 // iterates on path
-    var j  = 0 // iterates on stack
-    while (i < path.length) {
-      var key = path[i]
-      var c
-      j++
-
-      if (key && !key.recurse) {
-        c = (j === this.stack.length) ? this : this.stack[j]
-        if (!c) return
-        if (! check(key, c.key)) return
-        i++
-      } else {
-        i++
-        var nextKey = path[i]
-        if (! nextKey) return
-        while (true) {
-          c = (j === this.stack.length) ? this : this.stack[j]
-          if (!c) return
-          if (check(nextKey, c.key)) { i++; break}
-          j++
-        }
-      }
+    // Add ancestor keys
+    while( i < len ){
+      if( stack[i].key )
+        path.push(stack[i].key);
+      i++
     }
-    if (j !== this.stack.length) return
 
-    count ++
-    stream.queue(this.value[this.key])
-    delete this.value[this.key]
-  }
-  parser._onToken = parser.onToken;
+    // Add parent key and value
+    path.push(this.key, value);
+    
+    stream.queue(
+      handler ? handler.call(this, path)
+              : path
+    );
 
-  parser.onToken = function (token, value) {
-    parser._onToken(token, value);
-    if (this.stack.length === 0) {
-      if (stream.root) {
-        if(!path)
-          stream.queue(stream.root)
-        stream.emit('root', stream.root, count)
-        count = 0;
-        stream.root = null;
-      }
-    }
   }
+
 
   parser.onError = function (err) {
-    stream.emit('error', err)
+    stream.emit('error', err);
   }
 
 
-  return stream
-}
-
-function check (x, y) {
-  if ('string' === typeof x)
-    return y == x
-  else if (x && 'function' === typeof x.exec)
-    return x.exec(y)
-  else if ('boolean' === typeof x)
-    return x
-  else if ('function' === typeof x)
-    return x(y)
-  return false
-}
-
-exports.stringify = function (op, sep, cl) {
-  if (op === false){
-    op = ''
-    sep = '\n'
-    cl = ''
-  } else if (op == null) {
-
-    op = '[\n'
-    sep = '\n,\n'
-    cl = '\n]\n'
-
-  }
-
-  //else, what ever you like
-
-  var stream
-    , first = true
-    , anyData = false
-  stream = through(function (data) {
-    anyData = true
-    var json = JSON.stringify(data)
-    if(first) { first = false ; stream.queue(op + json)}
-    else stream.queue(sep + json)
-  },
-  function (data) {
-    if(!anyData)
-      stream.queue(op)
-    stream.queue(cl)
-    stream.queue(null)
-  })
-
-  return stream
-}
-
-exports.stringifyObject = function (op, sep, cl) {
-  if (op === false){
-    op = ''
-    sep = '\n'
-    cl = ''
-  } else if (op == null) {
-
-    op = '{\n'
-    sep = '\n,\n'
-    cl = '\n}\n'
-
-  }
-
-  //else, what ever you like
-
-  var stream = new Stream ()
-    , first = true
-    , anyData = false
-  stream = through(function (data) {
-    anyData = true
-    var json = JSON.stringify(data[0]) + ':' + JSON.stringify(data[1])
-    if(first) { first = false ; stream.queue(op + json)}
-    else stream.queue(sep + json)
-  }, 
-  function (data) {
-    if(!anyData) stream.queue(op)
-    stream.queue(cl)
-
-    stream.queue(null)
-  })
-
-  return stream
+  return stream;
 }
